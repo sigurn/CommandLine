7d8291274c2324f2dc2fec2efde0549de4c64fbb
Sigurn Lurk <sigurn.lurk@gmail.com>
2022-02-15 23:54:54 +0200

    Improve formatting

--- src/Sigurn.CommandLine.Sample/ArgsOptions.cs
+++ src/Sigurn.CommandLine.Sample/ArgsOptions.cs
@@ -1,33 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Sigurn.CommandLine.Sample;
 
-namespace Sigurn.CommandLine.Sample
+internal class ArgsOptions
 {
-    internal class ArgsOptions
+    [Argument(0, IsRequired = true)]
+    [HelpText("String argument")]
+    public string Arg1 { get; set; } = string.Empty;
+
+    [Argument(1)]
+    [HelpText("Integer argument")]
+    public int Arg2 { get; set; } = 123;
+
+    [Argument(2)]
+    [HelpText("String list argument")]
+    public IReadOnlyList<string> Arg3 { get; set; } = new List<string>();
+
+    [HelpText("Test arguments")]
+    public static Task Arguments(ArgsOptions options, CancellationToken cancellationToken)
     {
-        [Argument(0, IsRequired = true)]
-        [HelpText("String argument")]
-        public string Arg1 { get; set; } = string.Empty;
+        Console.WriteLine($"Arg1: {options.Arg1}");
+        Console.WriteLine($"Arg2: {options.Arg2}");
+        Console.WriteLine("Arg3: {0}", string.Join(", ", options.Arg3));
 
-        [Argument(1)]
-        [HelpText("Integer argument")]
-        public int Arg2 { get; set; } = 123;
-
-        [Argument(2)]
-        [HelpText("String list argument")]
-        public IReadOnlyList<string> Arg3 { get; set; } = new List<string>();
-
-        [HelpText("Test arguments")]
-        public static Task Arguments(ArgsOptions options, CancellationToken cancellationToken)
-        {
-            Console.WriteLine($"Arg1: {options.Arg1}");
-            Console.WriteLine($"Arg2: {options.Arg2}");
-            Console.WriteLine("Arg3: {0}", string.Join(", ", options.Arg3));
-
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
+
--- src/Sigurn.CommandLine.Sample/GreetingOptions.cs
+++ src/Sigurn.CommandLine.Sample/GreetingOptions.cs
@@ -1,39 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Sigurn.CommandLine.Sample;
 
-namespace Sigurn.CommandLine.Sample
+internal class GreetingOptions
 {
-    internal class GreetingOptions
+    [Argument(0)]
+    public string Name { get; set; } = "Anonymous";
+
+    [Option('s', "short")]
+    [HelpText("Show short version of the message")]
+    public bool IsShort { get; set; }
+
+    [HelpText("Say hello")]
+    public static void Hello(GreetingOptions options)
     {
-        [Argument(0)]
-        public string Name { get; set; } = "Anonymous";
+        if (options.IsShort)
+            Console.WriteLine($"Hi {options.Name}!");
+        else
+            Console.WriteLine($"Hello {options.Name}!");
+    }
 
-        [Option('s', "short")]
-        [HelpText("Show short version of the message")]
-        public bool IsShort { get; set; }
+    [CommandName("goodbye")]
+    [HelpText("Say goodbye")]
+    public static Task GoodbyeAsync(GreetingOptions options, CancellationToken cancellationToken)
+    {
+        if (options.IsShort)
+            Console.WriteLine($"Bye {options.Name}!");
+        else
+            Console.WriteLine($"Goodbye {options.Name}!");
 
-        [HelpText("Say hello")]
-        public static void Hello(GreetingOptions options)
-        {
-            if (options.IsShort)
-                Console.WriteLine($"Hi {options.Name}!");
-            else
-                Console.WriteLine($"Hello {options.Name}!");
-        }
-
-        [CommandName("goodbye")]
-        [HelpText("Say goodbye")]
-        public static Task GoodbyeAsync(GreetingOptions options, CancellationToken cancellationToken)
-        {
-            if (options.IsShort)
-                Console.WriteLine($"Bye {options.Name}!");
-            else
-                Console.WriteLine($"Goodbye {options.Name}!");
-
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
+
